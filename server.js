08300c7b99640ec0468c5a4018b7b264d6ddb70c
Thonyzito <anthonytamayo9@gmail.com>
2025-07-07 19:57:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const cors = require("cors");
+const app = express();
+
 app.use(cors({
   origin: "https://thonyzito.github.io"  // o "*" para pruebas, pero mejor restringir
 }));
-
-const app = express();
 
 app.use(express.json());
 app.use(express.raw({ type: "video/mp4", limit: "100mb" }));
